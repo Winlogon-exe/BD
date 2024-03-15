b110e87c5deeefb1ecd4c26cd85d37be7b4693bb
Саркисян Арман Игоревич <roman.sarkisyan15@gmail.com>
2024-03-15 17:15:43 +0300

    Update loginlogic.cpp

--- loginlogic.cpp
+++ loginlogic.cpp
@@ -7,11 +7,20 @@ LoginLogic::LoginLogic()
 
 void LoginLogic::initMap()
 {
-
     funcmap[Login]  = [this](){ login(); };
     funcmap[Cancel] = [this](){ cancel(); };
 }
 void LoginLogic::setButtonState(QObject* button, StateButton state)
 {
     buttonStateMap[button] = state;
+}
+
+void LoginLogic::login()
+{
+
+}
+
+void LoginLogic::cancel()
+{
+
 }
